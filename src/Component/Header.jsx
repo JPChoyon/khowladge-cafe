bc77539286a15eb6c2db970aa9d7d4bf94c3b48b
JP Choyon <jpchoyonkhan65@gmail.com>
2023-09-12 18:15:51 +0600

    added pic and modified the json file

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,11 +1,19 @@
-
-
 const Header = () => {
-    return (
-        <div>
-            <h1 className="text-4xl">Knowladge Cafe</h1>
+  return (
+    <div>
+      <div className="nav-container flex justify-between">
+        <div className="left-con">
+            <h2 className="text-4xl font-black">Knowladge Cafe</h2>
         </div>
-    );
+        <div className="right-con">
+        <div className="w-10 rounded-full">
+          <img src="../../public/images/profile.png" />
+        </div>
+        </div>
+
+      </div>
+    </div>
+  );
 };
 
 export default Header;
